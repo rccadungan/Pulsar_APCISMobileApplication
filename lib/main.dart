d79b7f9fc29f063e1bfca6d8c215083282520420
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-10 15:03:22 +0800

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:pulsar/pages/assessment_page.dart';
 import 'package:pulsar/pages/clearance_page.dart';
+import 'package:pulsar/pages/grades_page.dart';
 import 'package:pulsar/pages/home_page.dart';
 import 'package:pulsar/pages/login_page.dart';
+import 'package:pulsar/pages/notification_page.dart';
+import 'package:pulsar/pages/otp_page.dart';
+import 'package:pulsar/pages/pre_registration_page.dart';
+import 'package:pulsar/pages/profile_page.dart';
+import 'package:pulsar/pages/schedule_page.dart';
+import 'package:pulsar/profile/profile_screen.dart';
 import 'package:pulsar/services/shared_service.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
 Widget defaultHome = new LoginPage();
 
@@ -33,11 +42,98 @@ class _MyAppState extends State<MyApp> {
           brightness: Brightness.light,
           primaryColor: Color(0xff35438c),
           accentColor: Color(0xffdeac4b)),
-      home: Clearance(),
+      home: defaultHome,
       routes: <String, WidgetBuilder>{
         '/home': (BuildContext context) => new HomePage(),
-        '/login': (BuildContext context) => new LoginPage()
+        '/login': (BuildContext context) => new LoginPage(),
+        '/myAppPage': (BuildContext context) => new MyAppPage(),
+        '/profile': (BuildContext context) => new ProfilePage(),
+        '/grades': (BuildContext context) => new GradesPage(),
+        '/preRegistration': (BuildContext context) => new PreRegistrationPage(),
+        '/clearance': (BuildContext context) => new ClearancePage(),
+        '/profileScreen': (BuildContext context) => new ProfileScreen(),
+        '/assessment': (BuildContext context) => new AssessmentPage(),
+        '/otp': (BuildContext context) => new OtpScreen()
       },
     );
+  }
+}
+
+//Bottom Nav Bar
+class MyAppPage extends StatefulWidget {
+  @override
+  _MyAppPageState createState() => _MyAppPageState();
+}
+
+class _MyAppPageState extends State<MyAppPage> {
+  //Initialize the variable _currentIndex for selectedIndex parameter.
+  int _currentIndex = 0;
+  final List pages = [
+    HomePage(),
+    SchedulePage(),
+    AssessmentPage(),
+    NotificationPage(),
+    ProfileScreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        bottomNavigationBar: BottomNavyBar(
+          backgroundColor: Color(0xFFF0F0F0),
+          selectedIndex: _currentIndex,
+          onItemSelected: (int index) {
+            setState(() => _currentIndex = index);
+          },
+          items: <BottomNavyBarItem>[
+            BottomNavyBarItem(
+              title: Text('Home'),
+              icon: Image.asset(
+                "assets/images/home.png",
+                width: 27.0,
+                height: 27.0,
+              ),
+              inactiveColor: Colors.grey,
+              activeColor: Color(0xff35438c),
+            ),
+            BottomNavyBarItem(
+              title: Text('Schedule'),
+              icon: Image.asset(
+                "assets/images/schedule.png",
+                width: 27.0,
+                height: 27.0,
+              ),
+              inactiveColor: Colors.grey,
+              activeColor: Color(0xff35438c),
+            ),
+            BottomNavyBarItem(
+              title: Text('Assessment'),
+              icon: Image.asset(
+                "assets/images/assessment.png",
+                width: 27.0,
+                height: 27.0,
+              ),
+              inactiveColor: Colors.grey,
+              activeColor: Color(0xff35438c),
+            ),
+            BottomNavyBarItem(
+              title: Text('Notifications'),
+              icon: Image.asset(
+                "assets/images/notification.png",
+                width: 27.0,
+                height: 27.0,
+              ),
+              inactiveColor: Colors.grey,
+              activeColor: Color(0xff35438c),
+            ),
+            BottomNavyBarItem(
+              title: Text('More'),
+              icon: Icon(Icons.more_horiz),
+              inactiveColor: Colors.grey,
+              activeColor: Color(0xff35438c),
+            ),
+          ],
+        ),
+        body: pages[_currentIndex]);
   }
 }
