6875fcd7e8ec291695456d80543b4ff739428dc5
Rhyss Brian Cadungan <rccadungan@student.apc.edu.ph>
2021-03-10 14:59:07 +0800

    created app's home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,32 +1,302 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:pulsar/services/shared_service.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pulsar'),
+        title: Image.asset('assets/images/pulsar_logo_home.png',
+            fit: BoxFit.cover, height: 120.00, width: 120.00),
+        backgroundColor: Colors.transparent,
         elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.logout,
-              color: Color(0xffffffff),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 0,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Hi, Ram!",
+                          style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.w900,
+                            color: Color(0XFF040406),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Here are some of the more convenient ways you can \nuse to pay your tuition fee. Pay with Pulsar!",
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          height: 100,
+                          width: 320,
+                          decoration: BoxDecoration(
+                            color: Color(0xff35438c).withOpacity(0.2),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          child: Center(
+                            child: Column(
+                              // add Column
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text('Pulsar Pay',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                      fontWeight: FontWeight.w900,
+                                      color: Colors.white,
+                                    )),
+                                RaisedButton(
+                                  color: Color(0xffdeac4b),
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushReplacementNamed('/otp');
+                                  },
+                                  child: Text('Try now!',
+                                      style: TextStyle(
+                                          fontSize: 12.0, color: Colors.white)),
+                                ), // your button beneath text
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
             ),
-            onPressed: () {
-              SharedService.logout(context);
-            },
           ),
-          SizedBox(
-            width: 10,
+          Positioned(
+            top: 185,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              height: MediaQuery.of(context).size.height - 245,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: ListView(
+                children: [
+                  buildTitleRow("TODAY CLASSES", 3),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  buildClassItem(),
+                  buildClassItem(),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  buildTitleRow("YOUR TASKS", 4),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        buildTaskItem(
+                            3, "The Basic of Typography II", Colors.red),
+                        buildTaskItem(3, "Design Psychology: Principle of...",
+                            Colors.green),
+                        buildTaskItem(3, "Design Psychology: Principle of...",
+                            Colors.green),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
-      backgroundColor: Color(0xff35438c),
-      body: Center(
-        child: Text('Home'),
+    );
+  }
+
+  Container buildTaskItem(int numDays, String courseTitle, Color color) {
+    return Container(
+      margin: EdgeInsets.only(right: 15),
+      padding: EdgeInsets.all(12),
+      height: 140,
+      width: 140,
+      decoration: BoxDecoration(
+        color: color.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Deadline",
+            style: TextStyle(fontSize: 10, color: Colors.grey),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Row(
+            children: [
+              Container(
+                height: 6,
+                width: 6,
+                decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Text(
+                "$numDays days left",
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: 100,
+            child: Text(
+              courseTitle,
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Row buildTitleRow(String title, int number) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        RichText(
+          text: TextSpan(
+              text: title,
+              style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+              children: [
+                TextSpan(
+                  text: "($number)",
+                  style: TextStyle(
+                      fontSize: 12,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.normal),
+                ),
+              ]),
+        ),
+        Text(
+          "See all",
+          style: TextStyle(
+              fontSize: 12,
+              color: Color(0XFF3E3993),
+              fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+  }
+
+  Container buildClassItem() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      padding: EdgeInsets.all(10),
+      height: 100,
+      decoration: BoxDecoration(
+        color: Color(0xFFF9F9FB),
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "07:00",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "AM",
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
+              ),
+            ],
+          ),
+          Container(
+            height: 100,
+            width: 1,
+            color: Colors.grey.withOpacity(0.5),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width - 160,
+                child: Text(
+                  "MCSPROJ",
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.location_on,
+                    color: Colors.grey,
+                    size: 20,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 160,
+                    child: Text(
+                      "Room 504",
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(color: Colors.grey, fontSize: 13),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    "Gabriel Sutton",
+                    style: TextStyle(color: Colors.grey, fontSize: 13),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
